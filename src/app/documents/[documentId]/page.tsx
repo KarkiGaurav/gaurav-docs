7bcc5fa6592620491a5f0d299d55cf37eb1de305
Gaurav Singh Karki <karkigauravsingh@gmail.com>
2025-01-19 22:18:46 +0530

    make navbar functional

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -15,9 +15,10 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
-      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 right-0 left-0 z-10 bg-[#FAFBFD] print:hidden"></div>
-      <Navbar />
-      <Toolbar />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 right-0 left-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
       <div className="pt-[114px] print:pt-0">
         <Editor />
       </div>
