6d54f1ea85acb3ab3111a3c2b5559255acf58c60
Gaurav Singh Karki <karkigauravsingh@gmail.com>
2025-01-20 23:09:31 +0530

    search input added

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import SearchInput from './searchInput'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center h-full w-full'>
+      <div className="flex gap-3 items-center shrink-0 pr-6">
+        <Link href={'/'}>
+          <Image src="/logo.png" alt="Logo" width={36} height={36} />
+        </Link>
+
+        <h3 className="text-xl">
+          Docs
+        </h3>
+      </div>
+
+      <SearchInput />
+      
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./navbar";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="fixed top-0 right-0 left-0 h-16px bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+
+      </div>
+    </div>
+   
+  );
+}
